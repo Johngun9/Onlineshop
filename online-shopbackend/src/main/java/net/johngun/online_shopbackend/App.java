521e5e73a91e09e39023cacd734e85d9746e14aa
john <yohan.bs007@gmail.com>
2018-03-19 12:03:57 +0530

    My online-shopbackend app setup completed successfully

---
+++ online-shopbackend/src/main/java/net/johngun/online_shopbackend/App.java
@@ -0,0 +1,13 @@
+package net.johngun.online_shopbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
